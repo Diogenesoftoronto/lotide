f69905071c69bc1a00489c4766641c57fbb81fd8
Diogenesoftoronto <diogenesoft@protonmail.com>
2021-11-23 00:46:02 +0000

    expectation versus reality

---
+++ assertEqual.js
@@ -0,0 +1,9 @@
+const assertEqual = (actual, expected) => {
+  if (actual === expected) {
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+assertEqual("Loss","Win")
+assertEqual(1,1)
