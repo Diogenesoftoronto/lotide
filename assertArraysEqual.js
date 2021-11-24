b6e1b1ab66c9dc6da22b215260399ca02df7f3aa
diogenesoftoronto <diogenesoft@protonmail.com>
2021-11-23 20:41:37 -0500

    removed imports

---
+++ assertArraysEqual.js
@@ -0,0 +1,24 @@
+// import the eqArrays function from the "eqArrays.js" module
+
+const eqArrays = (arr1, arr2) => {
+  // check if the array lengths are different
+  if (arr1.length!== arr2.length) return false;
+  // loop through elements in an array to each other
+  for (let i = 0, j = 0; i < arr1.length; j++,i++) {
+    // check whether the two elements of the array are equal
+    if (arr1[i] === arr2[j]) {
+      continue;
+    } else {
+      return false;
+    }
+  }
+  return true;
+};
+
+const assertArraysEqual = (arr1, arr2) => {
+  if (eqArrays(arr1, arr2)) return console.log(`✅✅✅ Assertion Passed: ${arr1} === ${arr2}`);
+  else return console.log(`🛑🛑🛑 Assertion Failed: ${arr1} !== ${arr2}`);
+};
+assertArraysEqual() // => Assertion Passed
+assertArraysEqual([1, 2, 3, 4], [1, 2, 3, 4]); // => Assertion Passed
+assertArraysEqual(['1', '2', '3',], [1, 2]) // => Assertion Failed
