a78feee1f8f0dc74d8420236bb5f7c0c5dbe19b1
diogenesoftoronto <diogenesoft@protonmail.com>
2021-11-23 21:50:45 -0500

    finally uwu

--- without.js
+++ without.js
@@ -1,3 +1,18 @@
-const without = (sourceArr, itemToRemove) => {
-  // 
+const without = (sourceArr, itemsToRemove) => {
+  // loop through elements in the source array
+  let result = sourceArr
+  for(let j = 0; j < itemsToRemove.length; j++) {
+    let item = itemsToRemove[j]
+    if (sourceArr.includes(item)) {
+      let indexOfItem = sourceArr.indexOf(item)
+      sourceArr.splice(indexOfItem, 1)
+    } else {
+      return result;
+    }
+  }
+  return result;
 }
+
+const words = ["hello", "world", "lighthouse"];
+
+console.log(without(words, ["lighthouse"]));
