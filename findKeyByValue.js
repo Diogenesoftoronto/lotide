14d9b865b2c55298bcd4876c650947f5a77824b9
diogenesoftoronto <diogenesoft@protonmail.com>
2021-11-24 21:55:28 -0500

    changes abound

---
+++ findKeyByValue.js
@@ -0,0 +1,31 @@
+const assertEqual = (actual, expected) => {
+  if (actual === expected) {
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+// Implement the function findKeyByValue which takes in an object and a value. 
+// It should scan the object and return the first key which contains the given value. 
+// If no key with that given value is found, then it should return undefined.
+const findKeyByValue = (obj, val) => { 
+  // definitions
+  let result;
+
+  for (key in obj){
+    (obj[key] === val) ?  result = key: undefined;
+  }
+  return result;
+}
+
+
+
+const bestTVShowsByGenre = { 
+  sci_fi: "The Expanse",
+  comedy: "Brooklyn Nine-Nine",
+  drama:  "The Wire"
+};
+
+assertEqual(findKeyByValue(bestTVShowsByGenre, "The Wire"), "drama");
+assertEqual(findKeyByValue(bestTVShowsByGenre, "That '70s Show"), undefined);
