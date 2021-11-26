608a4c0de3febb95efedbe7461c78f07c6aeddf2
diogenesoftoronto <diogenesoft@protonmail.com>
2021-11-25 20:35:28 -0500

    finally works

--- takeUntil.js
+++ takeUntil.js
@@ -1,28 +1,20 @@
 // Creates a slice of array with elements taken from the beginning. Elements are taken until predicate returns falsey. The predicate is invoked with three arguments: (value, index, array).
-const takeWhile = (array, callback, value, index) => {
+const takeUntil = (array, callback, value, index) => {
   const results = [];
 
   if (index === undefined) index = 0;
 
-  (value !== undefined) ? value = array[index]: array[index] = value;
+  (value === undefined) ? value = array[index]: array[index] = value;
 
   if(callback===undefined) throw new Error('callback is undefined');
 
   if (array === undefined) throw new Error('array is undefined');
-
-    if (callback(value, index, array)) {
-
       for (value of array) {
-
-        if(callback(array, value, index)){
-
-          results = array.push(value);
-
+        if(!callback(value)){
+          results.push(value);
         } else break;
-
-        return results;
-    }
-  }
+      }
+      return results;
 }
 
 // const takeWhile = (array, callback) => {
