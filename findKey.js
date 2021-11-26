6c0f6bc8834c9ed7ba23abf8c5467cb9307b023c
diogenesoftoronto <diogenesoft@protonmail.com>
2021-11-25 20:47:31 -0500

    done for the day

---
+++ findKey.js
@@ -0,0 +1,27 @@
+const findKey = (obj, callback) => {
+  for (let key in obj) {
+    if (callback(obj[key])) {
+      return key;
+    }
+  }
+};
+
+// call function findKey
+results = findKey({
+  "Blue Hill": { stars: 1 },
+  "Akaleri":   { stars: 3 },
+  "noma":      { stars: 2 },
+  "elBulli":   { stars: 3 },
+  "Ora":       { stars: 2 },
+  "Akelarre":  { stars: 3 }
+}, x => x.stars === 2)
+
+const assertEqual = (actual, expected) => {
+  if (actual === expected) {
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+assertEqual(results, "noma");
