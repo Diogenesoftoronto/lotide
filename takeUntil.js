d4ce7969ee0be878d3ea6a970593ec8149644f53
diogenesoftoronto <diogenesoft@protonmail.com>
2021-11-25 19:59:46 -0500

    added test

--- takeUntil.js
+++ takeUntil.js
@@ -42,3 +42,13 @@ const takeWhile = (array, callback, value, index) => {
 // ];
  
 // takeWhile(users, function(o) { return !o.active; });
+
+const data1 = [1, 2, 5, 7, 2, -1, 2, 4, 5];
+const results1 = takeUntil(data1, x => x < 0);
+console.log(results1);
+
+console.log('---');
+
+const data2 = ["I've", "been", "to", "Hollywood", ",", "I've", "been", "to", "Redwood"];
+const results2 = takeUntil(data2, x => x === ',');
+console.log(results2);
